e894e1a78cbe4af2a3049e3af89610c0c3d98dc7
Anghel Andrei <mirceanghel98@gmail.com>
2026-06-14 16:48:37 +0300

    Backend fully done and functional. No other changes needed

--- backend/market-data-service/src/main/java/com/anghel/investmenthelper/market/exception/YahooFinanceException.java
+++
@@ -1,7 +0,0 @@
-package com.anghel.investmenthelper.market.exception;
-
-public class YahooFinanceException extends RuntimeException {
-    public YahooFinanceException(String message, Exception cause) {
-        super(message);
-    }
-}
